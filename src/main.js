5f1acc15a2d914e7b1b2858e7aa108ab63572669
Andrej Kautsevich <andrej.kautsevich@yandex.by>
2024-08-05 03:18:41 +0300

    feat: add download to PDF button

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import html2pdf from 'html2pdf.js';
+
 function saveData() {
   const elements = document.querySelectorAll('[contenteditable]');
   const data = {};
@@ -26,4 +28,20 @@ loadData();
 const elements = document.querySelectorAll('[contenteditable]');
 elements.forEach((element) => {
   element.addEventListener('input', saveData);
+});
+
+const save2PdfBtn = document.getElementById('save-2-pdf-btn');
+
+save2PdfBtn.addEventListener('click', () => {
+  const cvElement = document.getElementById('cv-element');
+
+  const options = {
+    margin: 10,
+    filename: 'my-cv.pdf',
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 4 },
+    jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }, // PDF settings
+  };
+
+  html2pdf().set(options).from(cvElement).save();
 });
